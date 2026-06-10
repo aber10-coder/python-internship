e3389a19a10f498becfec745776a4a2360812e50
Aber Nashid P <aberzxaberzx@gmail.com>
2026-06-10 15:47:50 +0530

    Day 2 internship tasks updated

---
+++ day2/main.py
@@ -0,0 +1,5 @@
+from utils import greet,calc_grade
+nm=(input("Enter your name:"))
+greet(nm)
+m=int(input("Enter you mark: "))
+calc_grade(m)
---
+++ day2/utils.py
@@ -0,0 +1,11 @@
+def greet(name):
+    print(f"Hi {name}")
+
+
+def calc_grade(mark):
+    if(mark>90):
+        print(" A ")
+    elif(mark>65 and mark<90):
+        print(" B ")
+    else:
+        print(" F ")
